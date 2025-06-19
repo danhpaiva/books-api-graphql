6e141b7a38e01c843405962977cb42eabc44e230
Paiva <danpaiva@live.com>
2025-06-19 16:30:29 -0300

    fix: corrigindo o formato GUID na hora de retornar o JSON

--- BooksApiGraphQl/BooksApiGraphQl/Controllers/Query.cs
+++ BooksApiGraphQl/BooksApiGraphQl/Controllers/Query.cs
@@ -6,8 +6,8 @@ public class Query
 {
     private static List<Book> books = new()
     {
-        new Book { Id = new Guid(), Title = "Code Clean", Author = "Robert C. Martin"},
-        new Book { Id = new Guid(), Title = "Domain-Driven Design", Author = "Eric Evans"},
+        new Book { Id = Guid.NewGuid(), Title = "Code Clean", Author = "Robert C. Martin"},
+        new Book { Id = Guid.NewGuid(), Title = "Domain-Driven Design", Author = "Eric Evans"},
     };
 
     public List<Book> GetBooks() => books;
