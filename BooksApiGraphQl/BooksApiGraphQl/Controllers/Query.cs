00293544fdf189bf8a482061217cbc6c7c0c0c96
Paiva <danpaiva@live.com>
2025-06-19 16:15:24 -0300

    feat: add class Query

---
+++ BooksApiGraphQl/BooksApiGraphQl/Controllers/Query.cs
@@ -0,0 +1,14 @@
+﻿using BooksApiGraphQl.Models;
+
+namespace BooksApiGraphQl.Controllers;
+
+public class Query
+{
+    private static List<Book> books = new()
+    {
+        new Book { Id = new Guid(), Title = "Code Clean", Author = "Robert C. Martin"},
+        new Book { Id = new Guid(), Title = "Domain-Driven Design", Author = "Eric Evans"},
+    };
+
+    public List<Book> GetBooks() => books;
+}
